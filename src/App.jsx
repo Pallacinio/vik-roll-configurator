f25607f52b698f73afa3cd3425afcf29367ac49f
WebTalkTeam <bartosz.palacz@webtalk.pl>
2025-02-25 21:44:22 +0100

    pathname for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function Layout() {
 
   return (
     <>
-      <Header />
+      {location.pathname !== "/" && <Header />}
       <Content>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -32,7 +32,7 @@ function Layout() {
           <Route path="/login" element={<Login />} />
         </Routes>
       </Content>
-      <BackButton />
+      {location.pathname !== "/" && <BackButton />}
       {location.pathname !== "/" && <Footer />}
     </>
   );
